54f40417f6b3e5998d83830a3d3ef395fd3fb36b
Chakree Kaewchai <chakree30584@gmail.com>
2015-10-22 01:15:39 +0700

    แก้บั้คชื่อแอดมินห้ามซ้ำกัน

--- admin/admin_adduser_save.php
+++ admin/admin_adduser_save.php
@@ -1,12 +1,17 @@
 <?php
-
 include "session.inc.php";
 include "../dbcon.inc.php";
 $username = $con->real_escape_string($_POST["username"]);
 $password = md5($con->real_escape_string($_POST["password"]));
-$queryUser = $con->query("INSERT INTO `admin`(`admin_id`, `admin_username`, `admin_password`) VALUES (null,'$username','$password' )");
-
-?>
+$checkExists = $con->query("SELECT * FROM admin WHERE admin_username = '$username'");
+if ($checkExists->num_rows > 0) {
+    echo "<script>alert('Username already exists.');window.history.back();</script>";
+} else {
+    $queryUser = $con->query("INSERT INTO `admin`(`admin_id`, `admin_username`, `admin_password`) VALUES (null,'$username','$password' )");
+    ?>
 <script>
     document.location = "admin_manageuser.php";
-    </script>
+</script>
+<?php
+}
+?>
